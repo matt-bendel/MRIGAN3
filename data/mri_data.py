f23ddcef2ddbe2c7ad69e9e9b463b0c989f5ecdb
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2022-05-11 17:46:06 -0400

    Increased num epochs and added training script

--- data/mri_data.py
+++ data/mri_data.py
@@ -232,10 +232,10 @@ class SelectiveSliceData_Val(torch.utils.data.Dataset):
 
         random.shuffle(files)
 
-        num_files = (round(len(files)) if big_test else round(len(files)*0.3))
+        num_files = (round(len(files)*0.7) if big_test else round(len(files)*0.3))
         print(num_files)
 
-        f_testing_and_Val = sorted(files[0:num_files])
+        f_testing_and_Val = sorted(files[-num_files:]) if big_test else sorted(files[0:num_files])
 
         files = f_testing_and_Val
 
