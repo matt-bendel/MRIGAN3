7df6403948dc1daec042f1137d965427b9ce835a
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2022-05-13 17:26:40 -0400

    Increased num epochs and added training script

--- train.py
+++ train.py
@@ -154,19 +154,19 @@ def gif_im(true, gen_im, index, type, disc_num=False):
     generate_image(fig, true, gen_im, f'z {index}', 1, 2, 1, disc_num=False)
     im, ax = generate_error_map(fig, true, gen_im, f'z {index}', 2, 2, 1)
 
-    plt.savefig(f'/home/bendel.8/Git_Repos/temp-final-mri/gif_{type}_{index - 1}.png')
+    plt.savefig(f'/home/bendel.8/Git_Repos/full_scale_mrigan/MRIGAN3/gif_{type}_{index - 1}.png')
     plt.close()
 
 
 def generate_gif(type):
     images = []
     for i in range(8):
-        images.append(iio.imread(f'/home/bendel.8/Git_Repos/temp-final-mri/gif_{type}_{i}.png'))
+        images.append(iio.imread(f'/home/bendel.8/Git_Repos/full_scale_mrigan/MRIGAN3/gif_{type}_{i}.png'))
 
     iio.mimsave(f'variation_gif.gif', images, duration=0.25)
 
     for i in range(8):
-        os.remove(f'/home/bendel.8/Git_Repos/temp-final-mri/gif_{type}_{i}.png')
+        os.remove(f'/home/bendel.8/Git_Repos/full_scale_mrigan/MRIGAN3/gif_{type}_{i}.png')
 ######################
 
 def train(args):
