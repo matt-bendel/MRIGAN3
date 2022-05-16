ffb3551256240f0667a4647c7d52e0bd6b1cf154
Matt Bendel <bendel.8@osu.edu>
2022-05-16 13:06:48 -0400

    Stylegan implementation and evaluation logic

--- models/generators/stylegan.py
+++ models/generators/stylegan.py
@@ -10,8 +10,8 @@ class StyleGAN(nn.Module):
             "enc_cfg": {
                 "drips_channels": [64, 64, 64, 64],
                 "drips_depth": [14, 12, 10, 8],
-                "norm": "bias",
-                "drips_norm": "bias",
+                "norm": "instance",
+                "drips_norm": "instance",
                 "channels": [
                     16,
                     128,
