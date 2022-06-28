6f0e2638e1af34b7202ad7e8389ffeb6c7401ce9
Matt Bendel <bendel.8@osu.edu>
2022-06-28 02:15:37 -0400

    Added script to get langevin metrics for various numbers of code vectors

--- wrappers/our_gen_wrapper.py
+++ wrappers/our_gen_wrapper.py
@@ -11,7 +11,7 @@ from utils.get_mask import get_mask
 def load_best_gan(args):
     from utils.prepare_models import build_model, build_model_sg
     checkpoint_file_gen = pathlib.Path(
-        f'{args.checkpoint_dir}/generator_model.pt')
+        f'{args.checkpoint_dir}/generator_best_model.pt')
     checkpoint_gen = torch.load(checkpoint_file_gen, map_location=torch.device('cuda'))
 
     generator = build_model(args) if not args.stylegan else build_model_sg(args)
