02b715f8e9853078500bfe0daf3599a48f749a65
Matt Bendel <bendel.8@osu.edu>
2022-07-11 15:26:06 -0400

    Added email to test fail

--- evaluation_scripts/metrics.py
+++ evaluation_scripts/metrics.py
@@ -330,7 +330,7 @@ def get_metrics(args):
 
                     for k in range(5):
                         lamda_val = lamda_flat[k]
-                        eigenvector_val = Vh.transpose()[k, :].reshape((384, 384))
+                        eigenvector_val = Vh[k, :].reshape((384, 384))
                         plt.imshow(eigenvector_val, cmap="jet")
                         plt.title(f"Eigenvector for Eigenvalue: {lamda_val:.2f}")
                         plt.savefig(f"eigenvector_{k}.png")
