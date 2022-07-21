3174fffc81842f2cc101bf98c496ae024b864fa7
Matt Bendel <bendel.8@osu.edu>
2022-07-21 16:43:22 -0400

    Added email to test fail

--- train.py
+++ train.py
@@ -274,8 +274,8 @@ def train(args, bl=1):
             for k in range(y.shape[0] - 1):
                 gen_pred_loss += torch.mean(fake_pred[k + 1])
 
-            mult = 1.25
-            std_weight = 0 * np.sqrt(2 / (np.pi * args.num_z * (args.num_z + 1)))
+            mult = 0
+            std_weight = mult * np.sqrt(2 / (np.pi * args.num_z * (args.num_z + 1)))
             adv_weight = 1e-3
             l1_weight = 1
             g_loss = - adv_weight * gen_pred_loss.mean()
