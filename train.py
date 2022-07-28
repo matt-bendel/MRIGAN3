8d22de4b245132ac56c16093464f4ffc2ac6c315
Matt Bendel <bendel.8@osu.edu>
2022-07-28 08:36:59 -0400

    Added email to test fail

--- train.py
+++ train.py
@@ -432,7 +432,7 @@ def train(args, bl=1, adv_mult=0.0):
         save_model(args, epoch, G.gen, opt_G, best_loss, best_model, 'generator')
         save_model(args, epoch, D, opt_D, best_loss, best_model, 'discriminator')
 
-        if (epoch + 1 % 5) == 0:
+        if (epoch + 1 % 2) == 0:
             std_mult += tune_std_weight(psnr_diff)
 
 
