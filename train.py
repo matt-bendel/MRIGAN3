f347e4fcf6469b88ebd9e447765ee794241834f2
Matt Bendel <bendel.8@osu.edu>
2022-07-28 04:04:27 -0400

    Added email to test fail

--- train.py
+++ train.py
@@ -480,7 +480,7 @@ if __name__ == '__main__':
     #         send_mail("TESTING FAILED", "See terminal for failure cause.")
 
 
-    vals = [1e-1, 1e-2, 1e-3, 1e-4, 1e-5]
+    vals = [1e-3, 1e-1, 1e-2, 1e-4, 1e-5]
     for val in vals:
         args.checkpoint_dir = "/home/bendel.8/Git_Repos/full_scale_mrigan/MRIGAN3/trained_models/base"
         try:
