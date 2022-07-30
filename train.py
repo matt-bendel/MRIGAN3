427fc7372d7010f89451a80ad6626e87fbdda26d
Matt Bendel <bendel.8@osu.edu>
2022-07-29 20:42:52 -0400

    Added email to test fail

--- train.py
+++ train.py
@@ -200,7 +200,7 @@ def tune_std_weight(signed_diff):
     if np.abs(signed_diff) < 0.1:
         return 0 if np.sign(signed_diff) == 1 else -0.025
 
-    if np.abs(signed_diff) > 0.5:
+    if np.abs(signed_diff) > 0.35:
         return 0.1 if np.sign(signed_diff) == 1 else -0.1
 
     return signed_diff * 0.1
@@ -434,7 +434,7 @@ def train(args, bl=1, adv_mult=0.0):
         save_model(args, epoch, D, opt_D, best_loss, best_model, 'discriminator')
 
         # if (epoch + 1 % 2) == 0:
-        # std_mult += tune_std_weight(psnr_diff)
+        std_mult += tune_std_weight(psnr_diff)
 
 
 if __name__ == '__main__':
