434ab7b21cce6e0a2f3c304009c7987d53bec9f5
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2022-08-09 16:55:04 -0400

    Debugging issue

--- train.py
+++ train.py
@@ -282,9 +282,9 @@ def train(args, bl=1, adv_mult=0.0):
                 mult = adv_mult
 
             # TODO: std_mult *
-            std_weight = 1.4 * np.sqrt(2 / (np.pi * args.num_z * (args.num_z + 1)))
-            adv_weight = mult
-            # adv_weight = 1e-3
+            std_weight = adv_mult * np.sqrt(2 / (np.pi * args.num_z * (args.num_z + 1)))
+            # adv_weight = mult
+            adv_weight = 1e-3
             l1_weight = 1
             g_loss = - adv_weight * gen_pred_loss.mean()
             g_loss += l1_weight * F.l1_loss(avg_recon, x)  # - args.ssim_weight * mssim_tensor(x, avg_recon)
@@ -469,7 +469,7 @@ if __name__ == '__main__':
     args.in_chans = 16
     args.out_chans = 16
 
-    vals = [1.4]
+    vals = [1.8]
 
     for val in vals:
         args.checkpoint_dir = "/home/bendel.8/Git_Repos/full_scale_mrigan/MRIGAN3/trained_models/base"
