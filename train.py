22c79f5d38e3c05ffbde42dc9554f46aab9c99af
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2022-08-28 17:27:32 -0400

    Debugging issue

--- train.py
+++ train.py
@@ -524,7 +524,7 @@ if __name__ == '__main__':
     #         print(e)
     #         send_mail("TESTING FAILED", "See terminal for failure cause.")
 
-    vals = [1e-3, 1e-4]
+    vals = [1e-1]
     for val in vals:
         args.checkpoint_dir = "/home/bendel.8/Git_Repos/full_scale_mrigan/MRIGAN3/trained_models/base"
 
