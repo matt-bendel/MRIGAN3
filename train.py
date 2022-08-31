18629452e03640e6b5040fe0cde173aebaab30de
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2022-08-31 14:03:55 -0400

    Added new training logic for other methods

--- train.py
+++ train.py
@@ -523,7 +523,7 @@ if __name__ == '__main__':
     #         print(e)
     #         send_mail("TESTING FAILED", "See terminal for failure cause.")
 
-    vals = [1e-1]
+    vals = [1e-2]
     args.checkpoint_dir = "/home/bendel.8/Git_Repos/full_scale_mrigan/MRIGAN3/trained_models"
     for val in vals:
         try:
