a8e2f54883e1586e35890a347cda0180694a54e9
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2022-09-06 00:19:14 -0400

    Making changes for ASILOMAR experiments

--- compare_plot.py
+++ compare_plot.py
@@ -22,82 +22,6 @@ from skimage.metrics import peak_signal_noise_ratio, structural_similarity
 from utils.parse_args import create_arg_parser
 from wrappers.our_gen_wrapper import load_best_gan
 
-def generate_image(fig, target, image, method, image_ind, rows, cols, kspace=False, disc_num=False):
-    # rows and cols are both previously defined ints
-    ax = fig.add_subplot(rows, cols, image_ind)
-    if method != 'GT' and method != 'Std. Dev':
-        psnr_val = psnr(target, image)
-        snr_val = snr(target, image)
-        ssim_val = ssim(target, image)
-        if not kspace:
-            pred = disc_num
-            ax.set_title(
-                f'PSNR: {psnr_val:.2f}, SNR: {snr_val:.2f}\nSSIM: {ssim_val:.4f}, Pred: {pred * 100:.2f}% True') if disc_num else ax.set_title(
-                f'PSNR: {psnr_val:.2f}, SNR: {snr_val:.2f}\nSSIM: {ssim_val:.4f}')
-
-    if method == 'Std. Dev':
-        im = ax.imshow(image, cmap='viridis')
-        ax.set_xticks([])
-        ax.set_yticks([])
-    else:
-        if kspace:
-            image = image ** 0.4
-            target = target ** 0.4
-        im = ax.imshow(np.abs(image), cmap='gray', vmin=0, vmax=np.max(target))
-        ax.set_xticks([])
-        ax.set_yticks([])
-        ax.set_xlabel(method)
-
-    return im, ax
-
-
-def generate_error_map(fig, target, recon, method, image_ind, rows, cols, relative=False, k=1, kspace=False):
-    # Assume rows and cols are available globally
-    # rows and cols are both previously defined ints
-    ax = fig.add_subplot(rows, cols, image_ind)  # Add to subplot
-
-    # Normalize error between target and reconstruction
-    if kspace:
-        recon = recon ** 0.4
-        target = target ** 0.4
-
-    error = (target - recon) if relative else np.abs(target - recon)
-    # normalized_error = error / error.max() if not relative else error
-    if relative:
-        im = ax.imshow(k * error, cmap='bwr', origin='lower', vmin=-0.0001, vmax=0.0001)  # Plot image
-        plt.gca().invert_yaxis()
-    else:
-        im = ax.imshow(k * error, cmap='jet', vmax=1) if kspace else ax.imshow(k * error, cmap='jet', vmax=0.0001)
-
-    # Remove axis ticks
-    ax.set_xticks([])
-    ax.set_yticks([])
-
-    # Return plotted image and its axis in the subplot
-    return im, ax
-
-
-def gif_im(true, gen_im, index, type, disc_num=False):
-    fig = plt.figure()
-
-    generate_image(fig, true, gen_im, f'z {index}', 1, 2, 1, disc_num=False)
-    im, ax = generate_error_map(fig, true, gen_im, f'z {index}', 2, 2, 1)
-
-    plt.savefig(f'/home/bendel.8/Git_Repos/full_scale_mrigan/MRIGAN3/gif_{type}_{index - 1}.png')
-    plt.close()
-
-
-def generate_gif(type):
-    images = []
-    for i in range(32):
-        images.append(iio.imread(f'/home/bendel.8/Git_Repos/full_scale_mrigan/MRIGAN3/gif_{type}_{i}.png'))
-
-    iio.mimsave(f'variation_gif.gif', images, duration=0.25)
-
-    for i in range(32):
-        os.remove(f'/home/bendel.8/Git_Repos/full_scale_mrigan/MRIGAN3/gif_{type}_{i}.png')
-
-
 # def generate_image(fig, target, image, method, image_ind, rows, cols, kspace=False, disc_num=False):
 #     # rows and cols are both previously defined ints
 #     ax = fig.add_subplot(rows, cols, image_ind)
@@ -105,22 +29,11 @@ def generate_gif(type):
 #         psnr_val = psnr(target, image)
 #         snr_val = snr(target, image)
 #         ssim_val = ssim(target, image)
-#         if method != None:
-#             ax.set_title(method, size=10)
-#
-#         # padding = 5
-#         # ax.annotate(
-#         #     s=f'PSNR: {psnr_val:.2f}\nSNR: {snr_val:.2f}\nSSIM: {ssim_val:.4f}',
-#         #     fontsize='xx-small',
-#         #     xy=(0, 0),
-#         #     xytext=(padding - 1, -(padding - 1)),
-#         #     textcoords='offset pixels',
-#         #     # bbox=dict(facecolor='white', alpha=1, pad=padding),
-#         #     va='top',
-#         #     ha='right',
-#         # )
-#         ax.text(1, 0.8, f'PSNR: {psnr_val:.2f}\nSNR: {snr_val:.2f}\nSSIM: {ssim_val:.4f}', transform=ax.transAxes,
-#                 horizontalalignment='right', verticalalignment='center', fontsize='xx-small', color='yellow')
+#         if not kspace:
+#             pred = disc_num
+#             ax.set_title(
+#                 f'PSNR: {psnr_val:.2f}, SNR: {snr_val:.2f}\nSSIM: {ssim_val:.4f}, Pred: {pred * 100:.2f}% True') if disc_num else ax.set_title(
+#                 f'PSNR: {psnr_val:.2f}, SNR: {snr_val:.2f}\nSSIM: {ssim_val:.4f}')
 #
 #     if method == 'Std. Dev':
 #         im = ax.imshow(image, cmap='viridis')
@@ -130,15 +43,15 @@ def generate_gif(type):
 #         if kspace:
 #             image = image ** 0.4
 #             target = target ** 0.4
-#         ax.set_title(method, size=10)
-#         im = ax.imshow(np.abs(image), cmap='gray', vmin=0, vmax=np.max(image))
+#         im = ax.imshow(np.abs(image), cmap='gray', vmin=0, vmax=np.max(target))
 #         ax.set_xticks([])
 #         ax.set_yticks([])
+#         ax.set_xlabel(method)
 #
 #     return im, ax
 #
 #
-# def generate_error_map(fig, target, recon, image_ind, rows, cols, relative=False, k=1, kspace=False, title=None):
+# def generate_error_map(fig, target, recon, method, image_ind, rows, cols, relative=False, k=1, kspace=False):
 #     # Assume rows and cols are available globally
 #     # rows and cols are both previously defined ints
 #     ax = fig.add_subplot(rows, cols, image_ind)  # Add to subplot
@@ -154,16 +67,103 @@ def generate_gif(type):
 #         im = ax.imshow(k * error, cmap='bwr', origin='lower', vmin=-0.0001, vmax=0.0001)  # Plot image
 #         plt.gca().invert_yaxis()
 #     else:
-#         im = ax.imshow(k * error, cmap='jet', vmax=1) if kspace else ax.imshow(k * error, cmap='jet', vmin=0, vmax=0.0001)
+#         im = ax.imshow(k * error, cmap='jet', vmax=1) if kspace else ax.imshow(k * error, cmap='jet', vmax=0.0001)
 #
-#     if title != None:
-#         ax.set_title(title, size=10)
 #     # Remove axis ticks
 #     ax.set_xticks([])
 #     ax.set_yticks([])
 #
 #     # Return plotted image and its axis in the subplot
 #     return im, ax
+#
+#
+# def gif_im(true, gen_im, index, type, disc_num=False):
+#     fig = plt.figure()
+#
+#     generate_image(fig, true, gen_im, f'z {index}', 1, 2, 1, disc_num=False)
+#     im, ax = generate_error_map(fig, true, gen_im, f'z {index}', 2, 2, 1)
+#
+#     plt.savefig(f'/home/bendel.8/Git_Repos/full_scale_mrigan/MRIGAN3/gif_{type}_{index - 1}.png')
+#     plt.close()
+#
+#
+# def generate_gif(type):
+#     images = []
+#     for i in range(32):
+#         images.append(iio.imread(f'/home/bendel.8/Git_Repos/full_scale_mrigan/MRIGAN3/gif_{type}_{i}.png'))
+#
+#     iio.mimsave(f'variation_gif.gif', images, duration=0.25)
+#
+#     for i in range(32):
+#         os.remove(f'/home/bendel.8/Git_Repos/full_scale_mrigan/MRIGAN3/gif_{type}_{i}.png')
+
+
+def generate_image(fig, target, image, method, image_ind, rows, cols, kspace=False, disc_num=False):
+    # rows and cols are both previously defined ints
+    ax = fig.add_subplot(rows, cols, image_ind)
+    if method != 'GT' and method != 'Std. Dev':
+        psnr_val = psnr(target, image)
+        snr_val = snr(target, image)
+        ssim_val = ssim(target, image)
+        if method != None:
+            ax.set_title(method, size=10)
+
+        # padding = 5
+        # ax.annotate(
+        #     s=f'PSNR: {psnr_val:.2f}\nSNR: {snr_val:.2f}\nSSIM: {ssim_val:.4f}',
+        #     fontsize='xx-small',
+        #     xy=(0, 0),
+        #     xytext=(padding - 1, -(padding - 1)),
+        #     textcoords='offset pixels',
+        #     # bbox=dict(facecolor='white', alpha=1, pad=padding),
+        #     va='top',
+        #     ha='right',
+        # )
+        ax.text(1, 0.8, f'PSNR: {psnr_val:.2f}\nSNR: {snr_val:.2f}\nSSIM: {ssim_val:.4f}', transform=ax.transAxes,
+                horizontalalignment='right', verticalalignment='center', fontsize='xx-small', color='yellow')
+
+    if method == 'Std. Dev':
+        im = ax.imshow(image, cmap='viridis')
+        ax.set_xticks([])
+        ax.set_yticks([])
+    else:
+        if kspace:
+            image = image ** 0.4
+            target = target ** 0.4
+        ax.set_title(method, size=10)
+        im = ax.imshow(np.abs(image), cmap='gray', vmin=0, vmax=np.max(image))
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+    return im, ax
+
+
+def generate_error_map(fig, target, recon, image_ind, rows, cols, relative=False, k=1, kspace=False, title=None):
+    # Assume rows and cols are available globally
+    # rows and cols are both previously defined ints
+    ax = fig.add_subplot(rows, cols, image_ind)  # Add to subplot
+
+    # Normalize error between target and reconstruction
+    if kspace:
+        recon = recon ** 0.4
+        target = target ** 0.4
+
+    error = (target - recon) if relative else np.abs(target - recon)
+    # normalized_error = error / error.max() if not relative else error
+    if relative:
+        im = ax.imshow(k * error, cmap='bwr', origin='lower', vmin=-0.0001, vmax=0.0001)  # Plot image
+        plt.gca().invert_yaxis()
+    else:
+        im = ax.imshow(k * error, cmap='jet', vmax=1) if kspace else ax.imshow(k * error, cmap='jet', vmin=0, vmax=0.0001)
+
+    if title != None:
+        ax.set_title(title, size=10)
+    # Remove axis ticks
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    # Return plotted image and its axis in the subplot
+    return im, ax
 
 
 def get_colorbar(fig, im, ax, left=False):
@@ -355,10 +355,10 @@ def main(args):
                     ours_samples_np[z] = torch.tensor(S.H * gen_ours).abs().numpy()
                     adler_samples_np[z] = torch.tensor(S.H * gen_adler).abs().numpy()
 
-                    gif_im(gt_np, ours_samples_np[z], place, 'image')
-                    place += 1
+                    # gif_im(gt_np, ours_samples_np[z], place, 'image')
+                    # place += 1
 
-                generate_gif('image')
+                # generate_gif('image')
 
                 std_ours_np = np.std(ours_samples_np, axis=0)
                 std_adler_np = np.std(adler_samples_np, axis=0)
