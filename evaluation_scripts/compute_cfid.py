fec5ededd80933ac9dd53172e69c6ada05534267
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2022-10-14 18:34:59 -0400

    Eval

--- evaluation_scripts/compute_cfid.py
+++ evaluation_scripts/compute_cfid.py
@@ -43,8 +43,7 @@ def get_cfid(args, G, langevin=False, loader=False):
                              cuda=True,
                              args=args)
 
-    cfids = cfid_metric.get_cfid_debug()
-    exit()
+    cfids = cfid_metric.get_cfid_torch()
     print(f'CFID: {np.mean(cfids)} \\pm {np.std(cfids) / np.sqrt(33)}')
 
     return np.mean(cfids)
