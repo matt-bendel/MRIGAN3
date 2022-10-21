145f45286aa6fc84b40b8622449655fa0ed8c666
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2022-10-21 00:59:20 -0400

    Modifications for us

--- evaluation_scripts/fid/fid_metric_langevin.py
+++ evaluation_scripts/fid/fid_metric_langevin.py
@@ -199,8 +199,8 @@ class FIDMetric:
                         new_filename = recon_directory + filename + f'|langevin|slide_idx_{i}_R=4_sample={j}_outputs.pt'
                         recon_object = torch.load(new_filename)
 
-                        recon = complex_abs(recon_object['mvue'][0].permute(1, 2, 0)).cuda().unsqueeze(0).unsqueeze(0)
-                        zfr = recon_object['zfr'][0].cuda().unsqueeze(0).unsqueeze(0)
+                        recon = recon_object['mvue'][0].abs().cuda().unsqueeze(0).unsqueeze(0)
+                        zfr = recon_object['zfr'][0].abs().cuda().unsqueeze(0).unsqueeze(0)
                         image = self._get_embed_im(recon)
                         condition_im = self._get_embed_im(zfr)
 
