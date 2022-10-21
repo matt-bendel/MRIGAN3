3cfd0bc4b2e1de095147a6fde9a8a9ca14b6767e
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2022-10-21 11:42:16 -0400

    Modifications for us

--- evaluation_scripts/fid/fid_metric.py
+++ evaluation_scripts/fid/fid_metric.py
@@ -147,7 +147,7 @@ class FIDMetric:
 
         self.transforms = torch.nn.Sequential(
             # transforms.Resize(256),
-            transforms.Resize(224),
+            # transforms.Resize(224),
             # transforms.CenterCrop(224),
             transforms.ConvertImageDtype(torch.float),
             transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
