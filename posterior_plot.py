734ceb7a03540a71d1c9376552caa81e0966e920
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2022-10-22 14:50:14 -0400

    Modifications for us

--- posterior_plot.py
+++ posterior_plot.py
@@ -317,7 +317,7 @@ def create_posterior_sample_plots(sample, gt, plot_num):
 
     # sub region of the original image
     x1, x2, y1, y2 = 100, 160, 200, 140
-    ax2.annotate("", (0.5, 0.5), arrowprops=dict(arrowstyle="->", color="yellow"))
+    ax2.annotate("", (0.5, 0.5), arrowprops=dict(arrowstyle="->"))
     axins2.set_xlim(x1, x2)
     axins2.set_ylim(y1, y2)
     # # fix the number of ticks on the inset axes
