bd65a558dd57534828f99bf07ca4bcfcf6f2af17
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2022-11-20 17:17:23 -0500

    Policy stuff

--- train_policy.py
+++ train_policy.py
@@ -358,7 +358,7 @@ if __name__ == '__main__':
     np.random.seed(0)
     torch.manual_seed(0)
 
-    args.batch_size = 200
+    args.batch_size = 100
     args.in_chans = 16
     args.out_chans = 16
 
