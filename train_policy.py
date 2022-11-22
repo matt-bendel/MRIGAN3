ecd7d375de093074280cbeceb6bad789dfe73239
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2022-11-22 12:06:36 -0500

    Small changes

--- train_policy.py
+++ train_policy.py
@@ -284,7 +284,6 @@ def train(args):
 
                 # Local baseline
                 loss = -1 * (action_logprobs * (action_rewards - avg_reward)) / (actions.size(-1) - 1)
-                print(loss.shape)
                 loss = loss.sum(dim=1)
                 # Average over batch
                 # Divide by batches_step to mimic taking mean over larger batch
@@ -293,7 +292,7 @@ def train(args):
                 accum_loss += loss.item()
 
                 idx = random.randint(0, mask.shape[1] - 1)
-                mask = mask[:, idx:idx + 1, :, :, :, :]
+                mask = mask[:, idx:idx + 1, :, :, :, :].squeeze(1)
                 print(mask.shape)
                 recons = (1-mask.unsqueeze(1).repeat(1, 8, 1, 1, 1, 1))*recons + mask.unsqueeze(1).repeat(1, 8, 1, 1, 1, 1)*kspace.unsqueeze(1).repeat(1, 8, 1, 1, 1, 1)
                 exit()
@@ -305,36 +304,36 @@ def train(args):
             optimiser.step()
 
         # TODO: This, one full sampling trajectory for arbitrary batch element
-        # ind = 2
-        # with torch.no_grad():
-        #     for i, data in enumerate(dev_loader):
-        #         zf, gt, kspace, gt_mean, gt_std, mask = data
-        #         zf = zf[ind].cuda().unsqueeze(0)
-        #         gt = gt[ind].cuda().unsqueeze(0)
-        #         kspace = kspace[ind].cuda().unsqueeze(0)
-        #         gt_mean = gt_mean[ind].cuda().unsqueeze(0)
-        #         gt_std = gt_std[ind].cuda().unsqueeze(0)
-        #         mask = mask[ind].cuda().unsqueeze(0)
-        #
-        #         recons, base_score = compute_scores(G, kspace, mask, zf, gt_mean, gt_std)
-        #         for step in range(48):
-        #             policy_in = torch.zeros(recons.size(0), 16, 384, 384).cuda()
-        #             var_recons = torch.var(recons, dim=1)
-        #             policy_in[:, 0:8, :, :] = var_recons[:, :, :, :, 0]
-        #             policy_in[:, 8:16, :, :] = var_recons[:, :, :, :, 0]
-        #
-        #             policy, probs = get_policy_probs(model, policy_in, mask, num_traj=1)
-        #             if step == 0:
-        #                 actions = torch.multinomial(probs.squeeze(1), args.num_test_trajectories, replacement=True)
-        #             else:
-        #                 actions = policy.sample()
-        #             exit()
-        #
-        #             mask[0, :, :, actions[0, 0], :] = 1
-        #
-        #             recons = (1 - mask.unsqueeze(1).repeat(1, 8, 1, 1, 1, 1)) * recons + mask.unsqueeze(1).repeat(1, 8,1, 1,1,1) * kspace.unsqueeze(1).repeat(1, 8, 1, 1, 1, 1)
-        #
-        #         break
+        ind = 2
+        with torch.no_grad():
+            for i, data in enumerate(dev_loader):
+                zf, gt, kspace, gt_mean, gt_std, mask = data
+                zf = zf[ind].cuda().unsqueeze(0)
+                gt = gt[ind].cuda().unsqueeze(0)
+                kspace = kspace[ind].cuda().unsqueeze(0)
+                gt_mean = gt_mean[ind].cuda().unsqueeze(0)
+                gt_std = gt_std[ind].cuda().unsqueeze(0)
+                mask = mask[ind].cuda().unsqueeze(0)
+
+                recons, base_score = compute_scores(G, kspace, mask, zf, gt_mean, gt_std)
+                for step in range(48):
+                    policy_in = torch.zeros(recons.size(0), 16, 384, 384).cuda()
+                    var_recons = torch.var(recons, dim=1)
+                    policy_in[:, 0:8, :, :] = var_recons[:, :, :, :, 0]
+                    policy_in[:, 8:16, :, :] = var_recons[:, :, :, :, 0]
+
+                    policy, probs = get_policy_probs(model, policy_in, mask, num_traj=1)
+                    if step == 0:
+                        actions = torch.multinomial(probs.squeeze(1), args.num_test_trajectories, replacement=True)
+                    else:
+                        actions = policy.sample()
+                    exit()
+
+                    mask[0, :, :, actions[0, 0], :] = 1
+
+                    recons = (1 - mask.unsqueeze(1).repeat(1, 8, 1, 1, 1, 1)) * recons + mask.unsqueeze(1).repeat(1, 8,1, 1,1,1) * kspace.unsqueeze(1).repeat(1, 8, 1, 1, 1, 1)
+
+                break
 
         # scheduler.step()
 
