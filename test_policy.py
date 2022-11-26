9745ce992f36a36003e58cf0b82e57fa64ba48fc
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2022-11-26 12:29:15 -0500

    test for policy

--- test_policy.py
+++ test_policy.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import imageio as iio
 import sigpy as sp
 import sigpy.mri as mr
+from data.mri_data import SelectiveSliceData_Val
 
 ################
 from typing import Optional
