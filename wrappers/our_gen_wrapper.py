8fae0f6f113b029183bf8defbb3f4f68235f625a
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2022-12-26 21:50:48 -0500

    test for policy

--- wrappers/our_gen_wrapper.py
+++ wrappers/our_gen_wrapper.py
@@ -113,8 +113,9 @@ class GANWrapper:
         z = torch.randn(num_vectors, self.resolution, self.resolution, 2).cuda()
         noise_fft = fft2c_new(z)
         measured_noise = ifft2c_new(mask[:, 0, :, :, :] * noise_fft).permute(0, 3, 1, 2)
-        nonmeasured_noise = ifft2c_new((1 - mask[:, 0, :, :, :]) * noise_fft).permute(0, 3, 1, 2)
-        return torch.cat([measured_noise, nonmeasured_noise], dim=1)
+        return measured_noise
+        # nonmeasured_noise = ifft2c_new((1 - mask[:, 0, :, :, :]) * noise_fft).permute(0, 3, 1, 2)
+        # return torch.cat([measured_noise, nonmeasured_noise], dim=1)
         # return torch.randn(num_vectors, 2, self.resolution, self.resolution).cuda()
 
     def update_gen_status(self, val):
