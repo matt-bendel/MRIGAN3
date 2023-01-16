760edc8568953599d3f62b0170b0a662e8588f9f
Matt Bendel <mattbendel@Matts-MacBook-Pro-2.local>
2023-01-16 17:51:18 -0500

    bug

--- evaluate_langevin_test_gan.py
+++ evaluate_langevin_test_gan.py
@@ -207,14 +207,14 @@ if __name__ == '__main__':
     args.in_chans = 16
     args.out_chans = 16
 
-    # args.checkpoint_dir = "/home/bendel.8/Git_Repos/MRIGAN3/trained_models/cvpr_ours"
-    # main(args)
+    args.checkpoint_dir = "/home/bendel.8/Git_Repos/MRIGAN3/trained_models/cvpr_ours"
+    main(args)
 
     # args.checkpoint_dir = "/home/bendel.8/Git_Repos/MRIGAN3/trained_models/cvpr_ohayon"
     # main(args)
 
-    args.checkpoint_dir = "/home/bendel.8/Git_Repos/MRIGAN3/trained_models/cvpr_adler"
-    main(args)
+    # args.checkpoint_dir = "/home/bendel.8/Git_Repos/MRIGAN3/trained_models/cvpr_adler"
+    # main(args)
 
     # print('MEDIAN')
     # print('APSD: ', np.median(apsd_vals))
