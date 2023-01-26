b3d77264c9e2925866241f646d3ada68474921da
Matt Bendel <mattbendel@Matts-MBP-2.lan>
2023-01-26 17:37:26 -0500

    Two reaLIZATRIONS

--- wrappers/our_gen_wrapper.py
+++ wrappers/our_gen_wrapper.py
@@ -110,11 +110,19 @@ class GANWrapper:
 
     def get_noise(self, num_vectors, var, mask):
         # return torch.cuda.FloatTensor(np.random.normal(size=(num_vectors, self.args.latent_size), scale=1))
-        z = torch.randn(num_vectors, self.resolution, self.resolution, 2).cuda()
-        noise_fft = fft2c_new(z)
-        measured_noise = ifft2c_new(mask[:, 0, :, :, :] * noise_fft).permute(0, 3, 1, 2)
+        z_vals = []
+        measured_vals = []
+        for i in range(2):
+            z = torch.randn(num_vectors, self.resolution, self.resolution, 2).cuda()
+            noise_fft = fft2c_new(z)
+            measured_noise = ifft2c_new(mask[:, 0, :, :, :] * noise_fft).permute(0, 3, 1, 2)
+            z_vals.append(z.permute(0, 3, 1, 2))
+            measured_vals.append(measured_noise)
+        # z = torch.randn(num_vectors, self.resolution, self.resolution, 2).cuda()
+        # noise_fft = fft2c_new(z)
+        # measured_noise = ifft2c_new(mask[:, 0, :, :, :] * noise_fft).permute(0, 3, 1, 2)
         # nonmeasured_noise = ifft2c_new((1 - mask[:, 0, :, :, :]) * noise_fft).permute(0, 3, 1, 2)
-        return measured_noise, z.permute(0, 3, 1, 2)
+        return measured_vals, z_vals
 
         # return torch.cat([measured_noise, nonmeasured_noise], dim=1)
         # return torch.randn(num_vectors, 2, self.resolution, self.resolution).cuda()
