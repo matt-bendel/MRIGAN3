1531cb6205665842d34df61915d73d5b9b683c05
Matt Bendel <mattbendel@Matts-MacBook-Pro-2.local>
2023-05-11 10:20:45 -0400

    ESRGAN gen

--- evaluate_langevin.py
+++ evaluate_langevin.py
@@ -9,7 +9,7 @@ from evaluation_scripts.fid.embeddings import VGG16Embedding
 
 from typing import Optional
 from skimage.metrics import peak_signal_noise_ratio, structural_similarity
-from evaluation_scripts.fid.fid_metric_langevin import FIDMetric
+from evaluation_scripts.fid.fid_metric_langevin_avg import FIDMetric
 from evaluation_scripts.cfid.cfid_metric_langevin import CFIDMetric
 from data_loaders.prepare_data import create_data_loaders
 import lpips
